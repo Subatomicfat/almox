e1c6409223c8720bf2d976547c539affd78eb5ca
Nicolas Brito Schrauth <165103017+Subatomicfat@users.noreply.github.com>
2026-09-03 01:55:38 -0300

    Delete frontend/src directory

--- frontend/src/api/auditLogApi.js
+++
@@ -1,6 +0,0 @@
-import api from './axiosClient';
-
-export async function listAuditLog(params = {}) {
-  const { data } = await api.get('/audit-log', { params });
-  return data;
-}
--- frontend/src/api/dashboardApi.js
+++
@@ -1,6 +0,0 @@
-import api from './axiosClient';
-
-export async function getDashboardStats() {
-  const { data } = await api.get('/dashboard/stats');
-  return data;
-}
